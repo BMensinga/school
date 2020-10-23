8ecf4b8184f7c5302f105c0dddfc6973169c5d27
Tirakv <Timkoorevaar@hotmail.com>
2020-10-23 22:22:35 +0200

    Array + objects added with JS

--- isdq/medewerkers.js
+++ isdq/medewerkers.js
@@ -1,8 +1,24 @@
-var medewerkers = [
-    "Aaron Hank",
-    "Thomas Bracken",
-    "Marlon Brecht",
-    "George Elliot",
-    "Steven Erikson"
-]
-document.getElementById("first").innerHTML = medewerkers[0];
+// Array medewerkers
+let medewerkers = []
+
+// Objects medewerkers
+let medewerkerOne = {Firstname: 'Aaron', Lastname: 'Hank'};
+let medewerkerTwo = {Firstname: 'Thomas', Lastname: 'Bracken'};
+let medewerkerThree = {Firstname: 'Marlon', Lastname: 'Brecht'};
+let medewerkerFour = {Firstname: 'George', Lastname: 'Elliot'};
+let medewerkerFive = {Firstname: 'Steven', Lastname: 'Erikson'};
+
+// Push medewerkers
+medewerkers.push(medewerkerOne);
+medewerkers.push(medewerkerTwo);
+medewerkers.push(medewerkerThree);
+medewerkers.push(medewerkerFour);
+medewerkers.push(medewerkerFive);
+
+// ForLoop medewerkers
+for (let i = 0; i < medewerkers.length; i++) {
+    console.log(medewerkers[i]);
+}
+
+// Print medewerker in HTML
+document.querySelector("#first").innerHTML = medewerkerOne.Firstname + " " + medewerkerOne.Lastname;
