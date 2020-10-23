78471404d3271fb4c02ec8550bf806b639a83260
Tim Koorevaar <timkoorevaar@MacBook-Pro-van-Tim.local>
2020-10-23 20:45:39 +0200

    javascript added

---
+++ isdq/medewerkers.js
@@ -0,0 +1,8 @@
+var medewerkers = [
+    "Aaron Hank",
+    "Thomas Bracken",
+    "Marlon Brecht",
+    "George Elliot",
+    "Steven Erikson"
+]
+document.getElementById("first").innerHTML = medewerkers[0];
