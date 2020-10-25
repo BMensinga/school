3b7ad40ab646c1abc7e468cb33bd3250e41c9f52
Tim Koorevaar <timkoorevaar@MacBook-Pro-van-Tim.local>
2020-10-25 14:48:57 +0100

    Function added and a forloop for the medewerkers

--- isdq/medewerkers.js
+++ isdq/medewerkers.js
@@ -1,24 +1,32 @@
-// Array medewerkers
-let medewerkers = []
+// Create an empty array for medewerkers.
+const medewerkers = [];
 
-// Objects medewerkers
-let medewerkerOne = {Firstname: 'Aaron', Lastname: 'Hank'};
-let medewerkerTwo = {Firstname: 'Thomas', Lastname: 'Bracken'};
-let medewerkerThree = {Firstname: 'Marlon', Lastname: 'Brecht'};
-let medewerkerFour = {Firstname: 'George', Lastname: 'Elliot'};
-let medewerkerFive = {Firstname: 'Steven', Lastname: 'Erikson'};
+// Create 5 medewerker objects.
+let medewerker1 = {firstName: 'Aaron', lastName: 'Hank'};
+let medewerker2 = {firstName: 'Thomas', lastName: 'Bracken'};
+let medewerker3 = {firstName: 'Marlon', lastName: 'Brecht'};
+let medewerker4 = {firstName: 'George', lastName: 'Elliot'};
+let medewerker5 = {firstName: 'Steven', lastName: 'Erikson'};
 
-// Push medewerkers
-medewerkers.push(medewerkerOne);
-medewerkers.push(medewerkerTwo);
-medewerkers.push(medewerkerThree);
-medewerkers.push(medewerkerFour);
-medewerkers.push(medewerkerFive);
-
-// ForLoop medewerkers
-for (let i = 0; i < medewerkers.length; i++) {
-    console.log(medewerkers[i]);
+// Push all medewerkers objects into medewekers array.
+for (let i = 1; i <= 5; i++) {
+    medewerkers.push(eval("medewerker" + i));
 }
 
-// Print medewerker in HTML
-document.querySelector("#first").innerHTML = medewerkerOne.Firstname + " " + medewerkerOne.Lastname;
+// Return the full object in the array if it exists.
+function showFullObject(array, key) {
+    for (let i = 0; i < array.length; i++) {
+        if (key === i) {
+            let object = array[i];
+            return `Medewerker is ${object.firstName} ${object.lastName}`;
+        }
+        else {
+            return 'Value does not exist!';
+        }
+    }
+}
+
+// Print first medewerker in the HTML file.
+document.querySelector("#first").innerHTML = showFullObject(medewerkers, 0);
+
+
