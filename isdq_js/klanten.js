2784a39f4718cb0ef7e58d77b307664a0328485d
Stivma <stivenmarcosmanuel@Stivens-MacBook-Pro.local>
2020-11-10 04:37:50 +0100

    html + js klanten bijgewerkt

---
+++ isdq_js/klanten.js
@@ -0,0 +1,19 @@
+function Klant (voornaam, achternaam) {
+    this.voornaam = voornaam;
+    this.achternaam = achternaam;
+
+};
+// Create an array with 5 objects.
+var klanten = [
+    new Klant ('Tino', 'Joao'),
+    new Klant ('Antonio', 'Teca'),
+    new Klant ('Antonio', 'Frank'),
+    new Klant ('Denilson', 'Coelho'),
+    new Klant ('Giel', 'Deelen'),
+];
+
+console.log(klanten);
+
+
+    
+
