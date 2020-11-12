90a4128fe74a0c3fae5b0c6c77311c5873c92827
Bas <bmensinga72@live.nl>
2020-11-12 14:26:10 +0100

    Changed how the accesories are created

--- isdq_js/accessories.js
+++ isdq_js/accessories.js
@@ -1,32 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Create a empty array accessories.
-    const accessories = [];
-
-    // Create 5 accessory objects.
-    let accessory1 = {accessoryName: 'Helm', price: 32.00};
-    let accessory2 = {accessoryName: 'Fietstas', price: 14.99};
-    let accessory3 = {accessoryName: 'Voor lamp', price: 2.10};
-    let accessory4 = {accessoryName: 'Achter lamp', price: 2.50};
-    let accessory5 = {accessoryName: 'Bagage drager', price: 49.99};
-
-    // Push all accessories objects into accessories array.
-    for (let i = 1; i <= 5; i++) {
-        accessories.push(eval("accessory" + i));
+    function Accessory(accessoryName, price) {
+        this.accessory = accessoryName;
+        this.price = price;
     }
 
-    // Return the full object in the array if it exists.
-    function showFullObject(array, key) {
-        for (let i = 0; i < array.length; i++) {
-            if (key === i) {
-                let object = array[i];
-                return `Accessoire ${object.accessoryName} is \t&euro;${object.price},-`;
-            }
-            else {
-                return 'Value does not exist!';
-            }
-        }
-    }
+    let accessories = [
+        new Accessory ('Helm', 32.00),
+        new Accessory ('Fietstas', 14.99),
+        new Accessory ('Voor lamp',  2.10),
+        new Accessory ('Achter lamp', 2.50),
+        new Accessory ('Bagage drager', 49.99)
+    ];
 
-    // Print first accessory in the HTML file.
-    document.querySelector("#first").innerHTML = showFullObject(accessories, 0);
+    document.getElementById("first").innerHTML = `Accessoire ${accessories[0].accessory} is \t&euro;${accessories[0].price},-`;
 });
