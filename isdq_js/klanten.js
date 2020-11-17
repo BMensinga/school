cc062ebea4df6b7e94f88876561e7e8c90f5fd7a
Stivma <stivenmarcosmanuel@Stivens-MacBook-Pro.local>
2020-11-18 00:36:13 +0100

    klantgegevens bijgewerkt

--- isdq_js/klanten.js
+++ isdq_js/klanten.js
@@ -1,18 +1,26 @@
-function Klant (voornaam, achternaam) {
+function Klant (voornaam, achternaam, leeftijd, woonplaats) {
+    
     this.voornaam = voornaam;
     this.achternaam = achternaam;
+    this.leeftijd = leeftijd;
+    this.woonplaats = woonplaats;
 
 };
 // Create an array with 5 objects.
 var klanten = [
-    new Klant ('Tino', 'Joao'),
-    new Klant ('Antonio', 'Teca'),
-    new Klant ('Antonio', 'Frank'),
-    new Klant ('Denilson', 'Coelho'),
-    new Klant ('Giel', 'Deelen'),
+    new Klant ('Tino', 'Joao',33, 'Den Haag' ),
+    new Klant ('Antonio', 'Teca', 34, 'Den Haag'),
+    new Klant ('Antonio', 'Frank' 35, 'Rijswijk'),
+    new Klant ('Denilson', 'Coelho' 32, Rotterdam ),
+    new Klant ('Giel', 'Deelen', 32, 'Delf'),
 ];
 
-console.log(klanten);
+document.getElementById(klantVoornaam).innerHTML = klantArray[0].voornaam;
+document.getElementById(klantAchternaam).innerHTML = klantArray[0].achternaam;
+document.getElementById(klantLeeftijd).innerHTML = klantArray[0].leeftijd;
+document.getElementById(klantWoonplaats).innerHTML = klantArray[0].woonplaats;
+
+
 
 
     
