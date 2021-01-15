1789d2c0a1ac309e43e97feec32d886a9e61894d
Tirakv <Timkoorevaar@hotmail.com>
2021-01-15 17:26:02 +0100

    pages made

--- routes/medewerkersdb.js
+++ routes/medewerkersdb.js
@@ -6,36 +6,35 @@ const sql = require("mssql");
 const dbConn = require("../lib/db");
 const request = new sql.Request();
 
-module.exports = (params)=>{
-
+module.exports = (params) => {
   router.get("/", function (req, res, next) {
-    request.query(
-      "SELECT * FROM dbo.MedewerkersDB",
-    function (err, rows) {
-        if (err) {
-            req.flash("error", err);
+    request.query("SELECT * FROM dbo.medewerkers", function (err, rows) {
+      if (err) {
+        req.flash("error", err);
 
-            //render to views/medewerkersdb/medewerkersdb.ejs.
-            res.render("pages/medewerkersdb.ejs", {pageTitle:"MSSQL Data", data: ""});
-        } else {
-          //Function to transform output of query in proper JSON format
-          let newResults = [];
-          for (let key in rows) {
-            if (key === "recordsets") {
-              rows[key].forEach((arr) => {
-                arr.forEach((obj) => {
-                  Object.keys(obj).forEach((key) => {
-                    newResults.push(obj[key]);
-                  });
+        res.render("pages/medewerkersdb.ejs", { pageTitle: "test", data: "" });
+      } else {
+        let newResults = [];
+        for (let key in results) {
+          if (key === "recordsets") {
+            results[key].forEach((arr) => {
+              arr.forEach((obj) => {
+                Object.keys(obj).forEach((key) => {
+                  newResults.push(obj[key]);
                 });
               });
-            }
+            });
           }
+        }
 
         rows = JSON.parse(newResults);
 
-        res.render("pages/medewerkersdb.ejs", {pageTitle: "MSSQL Data", data: ""});
+        res.render("pages/medewerkersdb.ejs", {
+          pageTitle: "MSSQL Data",
+          data: rows,
         }
-    }
+      };
     );
-  };
+  });
+  return router;
+};
