074034fa54896eee7e1a208fc2888c99d8cdd83c
Tirakv <Timkoorevaar@hotmail.com>
2021-01-17 16:49:02 +0100

    added

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,8 @@ const medewerkersdbRoute = require("./medewerkersdb");
 const accessoiresRoute = require("./accessoires");
 const accessoiresdbRoute = require("./accessoiresdb");
 const fietsenRoute = require("./fietsen");
-<<<<<<< HEAD
 const klantenRoute = require("./klanten");
-=======
 const fietsendbRoute = require("./fietsendb");
->>>>>>> 2f0b79ae75a53e44cb45390056fc34a01f3ac715
 
 const router = express.Router();
 
@@ -23,11 +20,8 @@ module.exports = (params) => {
   router.use("/accessoires", accessoiresRoute(params));
   router.use("/accessoiresdb", accessoiresdbRoute(params));
   router.use("/fietsen", fietsenRoute(params));
-<<<<<<< HEAD
   router.use("/klanten", klantenRoute(params));
-=======
   router.use("/fietsendb", fietsendbRoute(params));
->>>>>>> 2f0b79ae75a53e44cb45390056fc34a01f3ac715
 
   return router;
 };
